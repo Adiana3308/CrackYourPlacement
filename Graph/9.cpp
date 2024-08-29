3a5b39a27ec3fffebd263c4fc63c9146d5902860
Aditi Ghate <107868821+Adiana3308@users.noreply.github.com>
2024-08-29 13:09:52 +0530

    Update 9.cpp

--- Graph/9.cpp
+++ Graph/9.cpp
@@ -1,84 +1,93 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 // User function template for C++
 
-class Solution {
-  public:
-      void func(int row, int col, vector<vector<int>> &mat, int n, vector<string>& ans, string path){
-
-        if(row==n-1&&col==n-1){
+class Solution{
+    private:
+    bool isSafe(int x,int y,int n,vector<vector<int>>&visited, vector<vector<int>>&m)
+    {
+        if((x>=0 && x<n) && (y>=0 && y<n) && visited[x][y]==0 && m[x][y]==1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    void solve(vector<vector<int>> &m, int n,vector<string>&ans,int x,int y,vector<vector<int>>visited,string path)
+    {
+        // reached the destination.
+        if(x==n-1 && y==n-1)
+        {
             ans.push_back(path);
             return;
         }
         
-        mat[row][col]=0;
+        visited[x][y]=1;
         
-        if(row>0 && mat[row-1][col]==1){
-            func(row-1, col, mat, n, ans, path+"U");
+        // down
+        int newx=x+1;
+        int newy=y;
+        if(isSafe(newx,newy,n,visited,m))
+        {
+            path.push_back('D');
+            solve(m,n,ans,newx,newy,visited,path);
+            path.pop_back();
         }
-        if(row<n-1 && mat[row+1][col]==1){
-            func(row+1,col, mat, n, ans, path+"D");
-        }
-        if(col<n-1 && mat[row][col+1]==1){
-            func(row, col+1, mat, n, ans, path+"R");
-        }
-        if(col>0 && mat[row][col-1]==1){
-            func(row, col-1, mat, n, ans, path+"L");
+        // left
+          newx=x;
+          newy=y-1;
+        if(isSafe(newx,newy,n,visited,m))
+        {
+            path.push_back('L');
+            solve(m,n,ans,newx,newy,visited,path);
+            path.pop_back();
         }
         
-        mat[row][col]=1;
+        // right
+          newx=x;
+          newy=y+1;
+        if(isSafe(newx,newy,n,visited,m))
+        {
+            path.push_back('R');
+            solve(m,n,ans,newx,newy,visited,path);
+            path.pop_back();
+        }
+        
+        // up
+          newx=x-1;
+          newy=y;
+        if(isSafe(newx,newy,n,visited,m))
+        {
+            path.push_back('U');
+            solve(m,n,ans,newx,newy,visited,path);
+            path.pop_back();
+        }
     }
-    
-    vector<string> findPath(vector<vector<int>> &mat) {
-        // Your code goes here
-        int n=mat.size();
-        vector<string> ans;
-        
-        if(mat[0][0]==0||mat[n-1][n-1]!=1){
-            return {"-1"};
+    public:
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        vector<string>ans;
+        if(m[0][0]==0)
+        {
+            return ans;
         }
+        int srcx=0;
+        int srcy=0;
         
-        func(0,0,mat,n,ans,"");
+        vector<vector<int>>visited=m;
         
-        if(ans.empty()){
-            return {"-1"};
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                visited[i][j]=0;
+            }
         }
-        
+        string path = "";
+        solve(m,n,ans,srcx,srcy,visited,path);
+        sort(ans.begin(),ans.end());
         return ans;
     }
 };
 
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<vector<int>> m(n, vector<int>(n, 0));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                cin >> m[i][j];
-            }
-        }
-        Solution obj;
-        vector<string> result = obj.findPath(m);
-        sort(result.begin(), result.end());
-        if (result.size() == 0)
-            cout << -1;
-        else
-            for (int i = 0; i < result.size(); i++)
-                cout << result[i] << " ";
-        cout << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
+    
