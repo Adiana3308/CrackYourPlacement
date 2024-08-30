67fc38aa3ef3a4259504c56dccd1ff55f62116f3
Aditi Ghate <107868821+Adiana3308@users.noreply.github.com>
2024-08-30 11:25:21 +0530

    Update 21.cpp

--- Graph/21.cpp
+++ Graph/21.cpp
@@ -1,4 +1,12 @@
- vector<int> adj[N];
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+	bool isPossible(int N,int P, vector<pair<int, int> >& prerequisites) {
+	     vector<int> adj[N];
          vector<int> InDeg(N,0);
          for(int i=0;i<P;i++)
          {
@@ -26,5 +34,33 @@
              }
          }
          return count==N?1:0;
-    }
+    
+	}
 };
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+    	int N, P;
+        vector<pair<int, int> > prerequisites;
+        cin >> N;
+        cin >> P;
+        for (int i = 0; i < P; ++i) {
+            int x, y;
+            cin >> x >> y;
+            prerequisites.push_back(make_pair(x, y));
+        }
+        // string s;
+        // cin>>s;
+        Solution ob;
+        if (ob.isPossible(N,P, prerequisites))
+            cout << "Yes";
+        else
+            cout << "No";
+        cout << endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
