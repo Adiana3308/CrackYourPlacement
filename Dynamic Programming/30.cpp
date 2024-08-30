3e9405393b0119e52e86724ae42c85ab348c30f6
Aditi Ghate <107868821+Adiana3308@users.noreply.github.com>
2024-08-30 11:18:06 +0530

    Update 30.cpp

--- Dynamic Programming/30.cpp
+++ Dynamic Programming/30.cpp
@@ -1,10 +1,11 @@
-    int superEggDrop(int K, int N) {
-        vector<vector<int>> dp(N + 1, vector<int>(K + 1, 0));
-        int m = 0;
-        while (dp[m][K] < N) {
-            m++;
-            for (int k = 1; k <= K; ++k)
-                dp[m][k] = dp[m - 1][k - 1] + dp[m - 1][k] + 1;
-        }
+class Solution {
+public:
+    int superEggDrop(int k, int n) {
+         vector<int> dp(K + 1, 0);
+        int m;
+        for (m = 0; dp[K] < N; m++)
+            for (int k = K; k > 0; --k)
+                dp[k] += dp[k - 1] + 1;
         return m;
     }
+};
